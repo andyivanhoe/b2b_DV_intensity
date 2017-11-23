cae01d8f7b17210853f965a85b0f98d044693c7d
Andy <Andy@10.250.0.54>
2017-11-23 16:39:01 +0000

    Enable edge buttons on clicking bottom button.

--- MATLAB/Krox20Analysis/on_bottom_but_press.m
+++ MATLAB/Krox20Analysis/on_bottom_but_press.m
@@ -10,12 +10,15 @@ function on_bottom_but_press(hObject, eventdata, handles, controls)
         set(zr, 'Enable', 'on');
     end
     
+    set(controls.hzradios(1), 'Value', 1);
+    set([controls.hledgebut, controls.hredgebut], 'Enable', 'on');
+    
     data.bottom_slice_index = round(current_z_ind);
     setappdata(controls.hfig, 'data', data);
+
     
-    set(controls.hzradios(1), 'Value', 1);
-    eventdata.NewValue = controls.hzradios(1);
-    on_z_slice_selection_changed(hObject, eventdata, [], controls);
+%     eventdata.NewValue = controls.hzradios(1);
+%     on_z_slice_selection_changed(hObject, eventdata, [], controls);
     
 %     data.edges = [data.edges; Edges()];
 
--- MATLAB/Krox20Analysis/on_z_pos_changed.m
+++ MATLAB/Krox20Analysis/on_z_pos_changed.m
@@ -7,37 +7,55 @@ function on_z_pos_changed(hObject, eventdata, handles, controls)
     %data.curr_c_plane = round(get(controls.hcsl, 'Value'));
     update_image(controls)
     
-    % enable edge selection only if a relevant frame is displayed
+    % Enable edge selection when top and bottom z-slices have been selected
+    
     edge_buts = [controls.hledgebut, controls.hredgebut];
-    rh_buts = [controls.hrh4but, controls.hrh6but];
     if (isfield(data, 'top_slice_index') && isfield(data, 'bottom_slice_index'))
         if (~isempty(data.top_slice_index) && ~isempty(data.bottom_slice_index))
             
             target_slices = round(data.bottom_slice_index + data.z_offsets * (data.top_slice_index - data.bottom_slice_index));
 
             if any(data.curr_z_plane == target_slices)
-                if any(strcmp(data.channel_names, 'Krox20'))
-                    rhombomere_im = bfGetPlane(data.czi_reader, ...
-                        data.czi_reader.getIndex(data.curr_z_plane - 1, ....
-                        find(strcmp(data.channel_names, 'Krox20')) - 1, 0) + 1);
-                    data = detect_rhombomeres(controls, data, rhombomere_im);
-                end
-                if strcmp(data.channel_names{data.curr_c_plane}, 'Krox20')
-                    set(rh_buts, 'Enable', 'on');
-                    set(edge_buts, 'Enable', 'off');
-                else
-                    set(edge_buts, 'Enable', 'on');
-                    set(rh_buts, 'Enable', 'off');
-                end
+                set(edge_buts, 'Enable', 'on');
             else
-                set([edge_buts rh_buts], 'Enable', 'off');
+                set(edge_buts, 'Enable', 'off');
             end
         else
-            set([edge_buts rh_buts], 'Enable', 'off');
+            set(edge_buts, 'Enable', 'on');
         end
-        
-    else
-        set([edge_buts rh_buts], 'Enable', 'off');
     end
+    
+%     % enable edge selection only if a relevant frame is displayed
+%     edge_buts = [controls.hledgebut, controls.hredgebut];
+%     rh_buts = [controls.hrh4but, controls.hrh6but];
+%     if (isfield(data, 'top_slice_index') && isfield(data, 'bottom_slice_index'))
+%         if (~isempty(data.top_slice_index) && ~isempty(data.bottom_slice_index))
+%             
+%             target_slices = round(data.bottom_slice_index + data.z_offsets * (data.top_slice_index - data.bottom_slice_index));
+% 
+%             if any(data.curr_z_plane == target_slices)
+%                 if any(strcmp(data.channel_names, 'Krox20'))
+%                     rhombomere_im = bfGetPlane(data.czi_reader, ...
+%                         data.czi_reader.getIndex(data.curr_z_plane - 1, ....
+%                         find(strcmp(data.channel_names, 'Krox20')) - 1, 0) + 1);
+%                     data = detect_rhombomeres(controls, data, rhombomere_im);
+%                 end
+% %                 if strcmp(data.channel_names{data.curr_c_plane}, 'Krox20')
+% %                     set(rh_buts, 'Enable', 'on');
+% %                     set(edge_buts, 'Enable', 'off');
+% %                 else
+% %                     set(edge_buts, 'Enable', 'on');
+% %                     set(rh_buts, 'Enable', 'off');
+% %                 end
+%             else
+%                 set([edge_buts rh_buts], 'Enable', 'off');
+%             end
+%         else
+%             set([edge_buts rh_buts], 'Enable', 'off');
+%         end
+%         
+%     else
+%         set([edge_buts rh_buts], 'Enable', 'off');
+%     end
     
     setappdata(controls.hfig, 'data', data);
