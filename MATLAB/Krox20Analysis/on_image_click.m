8a02ecb8037c245362b274de03fe928636ca629f
Andy <Andy@10.250.0.54>
2017-11-23 17:48:32 +0000

    Changed line drawing tool on image click to impoly - click to make a new anchor point.
    No errors.
    
    Current bug: nothing happens when I click on 'Use current edge as left basal surface'.

--- MATLAB/Krox20Analysis/on_image_click.m
+++ MATLAB/Krox20Analysis/on_image_click.m
@@ -1,3 +1,21 @@
+% function on_image_click(hObject, eventdata, handles, controls)
+% 
+%     data = getappdata(controls.hfig, 'data');
+%     
+%     kids = get(controls.hax, 'Children');
+%     delete(kids(strcmp(get(kids, 'Type'), 'hggroup')))
+% 
+%    %closed = strcmp(data.channel_names{data.curr_c_plane}, 'Krox20');
+%    % try
+%     data.current_edge = imfreehand(controls.hax, 'Closed');
+%    % end
+% %     data.current_edge = M1.getPosition;
+% 
+%     setappdata(controls.hfig, 'data', data);
+%     
+% end
+
+
 function on_image_click(hObject, eventdata, handles, controls)
 
     data = getappdata(controls.hfig, 'data');
@@ -5,12 +23,19 @@ function on_image_click(hObject, eventdata, handles, controls)
     kids = get(controls.hax, 'Children');
     delete(kids(strcmp(get(kids, 'Type'), 'hggroup')))
 
-    closed = strcmp(data.channel_names{data.curr_c_plane}, 'Krox20');
-    try
-        data.current_edge = imfreehand(controls.hax, 'Closed', closed);
-    end
-%     data.current_edge = M1.getPosition;
-
+    data.current_edge = impoly(controls.hax, 'Closed',false);
+    
+    cods = getPosition(data.current_edge);
+    
+    y = linspace(cods(1,2), cods(2,2)); % Extract y-values along the impoly line
+    
+    %%TODO: replace 42 with 
+    y2 = linspace(cods(1,2), cods(2,2), (cods(2,2) - cods(1,2)));    % Scale linspace to size of line
+    
+       
+    y3 = uint8(y2);    % Make integer for every y-pixel along the line
+    y3 = y3.';  % Transpose y-pixel values along impoly line from a row to a column
+    
     setappdata(controls.hfig, 'data', data);
     
-end
+end    
