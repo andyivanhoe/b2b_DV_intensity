c119d27c66b833bacac4dda89e8c44e3848cf802
Andy <Andy@10.250.0.54>
2017-11-23 14:24:16 +0000

    Removed channel slider. UI was crashing when input data only had one channel, when I moved this slider.

--- MATLAB/Krox20Analysis/initialise_sliders.m
+++ MATLAB/Krox20Analysis/initialise_sliders.m
@@ -3,7 +3,7 @@ function initialise_sliders(controls, data)
     data = getappdata(controls.hfig, 'data');
 
     zPlanes = double(data.ome_meta.getPixelsSizeZ(0).getValue());
-    cPlanes = double(data.ome_meta.getPixelsSizeC(0).getValue());
+    % cPlanes = double(data.ome_meta.getPixelsSizeC(0).getValue());
 
     set(controls.hzsl, 'Max', zPlanes);
     set(controls.hzsl, 'Min', 1);
@@ -14,17 +14,17 @@ function initialise_sliders(controls, data)
         sstep = 1;
     end
     set(controls.hzsl, 'SliderStep', [(1/sstep) (1/sstep)]);
-    
-    set(controls.hcsl, 'Max', cPlanes);
-    set(controls.hcsl, 'Min', 1);
-    set(controls.hcsl, 'Value', 1);
-    if cPlanes > 1
-        sstep = cPlanes - 1;
-    else
-        sstep = 1;
-    end
-    set(controls.hcsl, 'SliderStep', [(1/sstep) (1/sstep)]);
-
-    setappdata(controls.hfig, 'data', data);
-
 end
+    
+%% Commenting out all hcsl, channel slider, reference means it does not crash when I move hcsl with one-channel datasets    
+%     set(controls.hcsl, 'Max', cPlanes);
+%     set(controls.hcsl, 'Min', 1);
+%     set(controls.hcsl, 'Value', 1);
+%     if cPlanes > 1
+%         sstep = cPlanes - 1;
+%     else
+%         sstep = 1;
+%     end
+%     set(controls.hcsl, 'SliderStep', [(1/sstep) (1/sstep)]);
+% 
+%     setappdata(controls.hfig, 'data', data);
--- MATLAB/Krox20Analysis/update_image.m
+++ MATLAB/Krox20Analysis/update_image.m
@@ -3,10 +3,10 @@ function update_image(controls)
     data = getappdata(controls.hfig, 'data');
     
     current_z_ind = round(get(controls.hzsl, 'Value'));
-    current_c_ind = round(get(controls.hcsl, 'Value'));
+    %current_c_ind = round(get(controls.hcsl, 'Value'));
     
     im = bfGetPlane(data.czi_reader, ...
-        data.czi_reader.getIndex(current_z_ind - 1, current_c_ind - 1, 0) + 1);
+        data.czi_reader.getIndex(current_z_ind - 1, 0, 0) + 1);
     if get(controls.hhicontrast, 'Value')
         clims = [min(im(:)) max(im(:))/10];
     else
